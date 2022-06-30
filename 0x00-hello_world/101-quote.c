44777ae38f9f729853739285703573fbae6ada28
0508-mj <mukamijoan00@gmail.com>
2022-06-30 12:48:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,30 @@
-#include<unistd.h>
+#include <stdio.h>
+
+#include <string.h>
+
+#include <unistd.h>
 
 /**
  *
- *  * main - Entry point
+ *  * main - main block
  *
- *   *
+ *   * Return: 1
  *
- *    * Return: Always 1 (Success)
- *
- *     */
+ *    */
 
 int main(void)
 
 {
 
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-			
 
-			write(2, str1, 59);
+
+			write(2,
+
+						      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+
+						      	      s);
 
 				return (1);
 
