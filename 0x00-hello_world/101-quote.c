e766ba254850873d2652e5ebbf605585f86a389b
0508-mj <mukamijoan00@gmail.com>
2022-06-30 13:30:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-#include <string.h>
-
-
+#include <unistd.h>
 
 /**
  *
- *  * main - Entry point
+ *  * main - Prints out the last part of a quote in the standard error.
  *
- *   *
+ *   * Return: 1 if success.
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
 
 int main(void)
 
 {
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 
-		char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-			size_t len = strlen(str);
-
-				/* Starting main */
-
-				fwrite(str, len, 1, stderr);
-
-					return (1);
-
+			return (1);
 }
