09e7ec2903e70a8d2e6c0a240f9d942221c98001
0508-mj <mukamijoan00@gmail.com>
2022-07-01 10:58:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	puchar('\n');
+
+	return (0);
+}
+	
