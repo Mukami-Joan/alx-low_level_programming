601546db4e451c6c9ecf1b752f5d776e97829c54
0508-mj <mukamijoan00@gmail.com>
2022-07-01 09:44:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++);
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
