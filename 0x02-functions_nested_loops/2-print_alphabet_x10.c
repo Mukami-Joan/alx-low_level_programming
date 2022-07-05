d199ae74c0b1704106e7e73de198168cef4e7117
0508-mj <mukamijoan00@gmail.com>
2022-07-05 03:54:00 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char c;
+
+	while (count++ <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		putchar('\n');
+	}
+}
