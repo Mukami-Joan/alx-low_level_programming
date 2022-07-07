3b4527e5d3bb15edd5484d1625423dd9223f9c2f
0508-mj <mukamijoan00@gmail.com>
2022-07-07 05:56:18 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - printing numbers
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+	_putchar('\n');
+
+	return (0);
+}
