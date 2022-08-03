255efecb4cb4aabab64e47b54351a2b89d5ed016
Mukami <mukamijoan00@gmail.com>
2022-08-03 21:03:30 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,30 +1,20 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * get_op_func - select the correct operation function asked by user
- * @s: operator argument
- * Return: function pointer corresponding to operator given
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
-int (*get_op_func(char *s))(int, int)
+typedef struct op
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
-
-	i = 0;
-	while (i < 5)
-	{
-		if (ops[i].op[0] == s[0])
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
-}
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+#endif
