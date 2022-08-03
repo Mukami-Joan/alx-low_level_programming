b85575f85c974ed5f31abc946eaefa8ce130f057
Mukami <mukamijoan00@gmail.com>
2022-08-03 21:05:13 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,41 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: num of args
- * @argv: array of args
- *
- * Return: Always 0 (Success)
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int (*f)(int, int);
+	int (*o)(int, int);
 
 	if (argc != 4)
 	{
-		ERR_OUT;
-		ERR_NUM;
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	f = get_op_func(argv[2]);
 
-	if (!f || argv[2][1])
-	{
-		ERR_OUT;
-		ERR_OPP;
-	}
-
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
-	{
-		ERR_OUT;
-		ERR_INF;
-	}
-
-	printf("%d\n", f(a, b));
+	printf("%d\n", o(a, b));
 
 	return (0);
 }
