048a4ea6cb8bfc5046784a7dbddabe1f45942c04
Mukami <mukamijoan00@gmail.com>
2022-08-03 20:43:36 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,21 @@
+#ifndef _CALC_H
+#define _CALC_H
+
+#define ERR_NUM (exit(98))
+#define ERR_OPP (exit(99))
+#define ERR_INF (exit(100))
+
+#define ERR_OUT (printf("Error\n"))
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
 	char *op;
@@ -9,4 +27,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int);
+
+#endif /* CALC */
